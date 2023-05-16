36315ac5524f5b3a4fb5631ae07a7a6e45d92f15
DinaraDervel <dinaradervel@gmail.com>
2023-05-16 17:05:25 -0600

    add calculation of sums with discount

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,36 @@
-const arrayOfSums = document.getElementsByClassName('card__sum');
+const arrayOfSums = document.getElementsByClassName('card__sum'); //массив элементов со значениями сумм товаров
 let sumTotal = 0;
 for (let i = 0; i < arrayOfSums.length; i++)
     {
-     sumTotal += +arrayOfSums[i].textContent;
+     //получаем значение суммы
+     const valueOfSum = arrayOfSums[i].textContent;
+     //убираем разрядные разделители в виде запятых и приводим сумму к числу
+     const numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
+     //считаем итоговую сумму
+     sumTotal += numberOfSum;
     }
 const resultSum = document.querySelector(".result-block__total-sum");
-resultSum.textContent = sumTotal;
+resultSum.textContent = Intl.NumberFormat("en").format(sumTotal); //обратно добавляем разделители разрядов в итоговую сумму
+
+const applyDiscount = () => {
+    sumTotal = 0;
+    for (let i = 0; i < arrayOfSums.length; i++)
+    {
+     //получаем значение суммы
+     const valueOfSum = arrayOfSums[i].textContent;
+     //убираем разрядные разделители в виде запятых и приводим сумму к числу
+     let numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
+     //считаем сумму со скидкой
+     numberOfSum -= numberOfSum*0.2;
+     //выводим значение суммы со скидкой
+     arrayOfSums[i].textContent = Intl.NumberFormat("en").format(numberOfSum);
+     //считаем итоговую сумму со скидкой
+     sumTotal += numberOfSum;
+    }
+    //выводим значение итоговой суммы со скидкой
+    const resultSum = document.querySelector(".result-block__total-sum");
+    resultSum.textContent = Intl.NumberFormat("en").format(sumTotal);
+    //делаем кнопку не активной
+    const button = document.querySelector(".result-block__discount");
+    button.disabled = "true";
+};
