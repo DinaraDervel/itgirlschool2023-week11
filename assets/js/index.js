baba621eda3d41f4230f42c9f3ea0850dd29e5d0
DinaraDervel <dinaradervel@gmail.com>
2023-05-19 14:18:11 -0600

    add eslint and change method to disable button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,15 @@
 //функция вычисления суммы элементов массива сумм товаров
 const calcSumOfArray = (arrayOfSums) => {
-let sumTotal = 0;
-for (let i = 0; i < arrayOfSums.length; i++)
-    {
-     //получаем значение суммы
-     const valueOfSum = arrayOfSums[i].textContent;
-     //убираем разрядные разделители в виде запятых и приводим сумму к числу
-     const numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
-     //считаем итоговую сумму
-     sumTotal += numberOfSum;
+    let sumTotal = 0;
+    for (let i = 0; i < arrayOfSums.length; i++) {
+        //получаем значение суммы
+        const valueOfSum = arrayOfSums[i].textContent;
+        //убираем разрядные разделители в виде запятых и приводим сумму к числу
+        const numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
+        //считаем итоговую сумму
+        sumTotal += numberOfSum;
     }
-return sumTotal;
+    return sumTotal;
 }
 //массив элементов со значениями сумм товаров
 const arrayOfSums = document.getElementsByClassName('card__sum');
@@ -21,21 +20,20 @@ resultSum.textContent = Intl.NumberFormat("en").format(calcSumOfArray(arrayOfSum
 //функция применения скидки
 const applyDiscount = () => {
     sumTotal = 0;
-    for (let i = 0; i < arrayOfSums.length; i++)
-    {
-     //получаем значение суммы
-     const valueOfSum = arrayOfSums[i].textContent;
-     //убираем разрядные разделители в виде запятых и приводим сумму к числу
-     let numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
-     //считаем сумму со скидкой
-     numberOfSum -= numberOfSum*0.2;
-     //выводим значение суммы со скидкой
-     arrayOfSums[i].textContent = Intl.NumberFormat("en").format(numberOfSum);
+    for (let i = 0; i < arrayOfSums.length; i++) {
+        //получаем значение суммы
+        const valueOfSum = arrayOfSums[i].textContent;
+        //убираем разрядные разделители в виде запятых и приводим сумму к числу
+        let numberOfSum = parseFloat(valueOfSum.replaceAll(',', ''));
+        //считаем сумму со скидкой
+        numberOfSum -= numberOfSum * 0.2;
+        //выводим значение суммы со скидкой
+        arrayOfSums[i].textContent = Intl.NumberFormat("en").format(numberOfSum);
     }
     //выводим значение итоговой суммы со скидкой
     const resultSum = document.querySelector(".result-block__total-sum");
     resultSum.textContent = Intl.NumberFormat("en").format(calcSumOfArray(arrayOfSums));
     //делаем кнопку не активной
     const button = document.querySelector(".result-block__discount");
-    button.disabled = "true";
+    button.setAttribute('disabled', true);
 };
